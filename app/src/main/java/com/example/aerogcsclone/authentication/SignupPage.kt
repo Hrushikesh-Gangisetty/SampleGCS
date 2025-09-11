99f829cc0696304c430ec1307d061a7470631b26
AkhilaSirumalle <AkhilaSirumalle>
2025-09-11 16:10:29 +0530

    WIP: local changes before pulling

--- app/src/main/java/com/example/aerogcsclone/authentication/SignupPage.kt
+++ app/src/main/java/com/example/aerogcsclone/authentication/SignupPage.kt
@@ -1,6 +1,6 @@
 package com.example.aerogcsclone.authentication
 
-import android.widget.Toast
+//import android.widget.Toast
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
